4eee1acaf9ab28283d8b893a891b923204124827
Anticubex <teofontana00@gmail.com>
2025-09-23 15:52:07 -0700

    2 Motors being tested now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SIGMA/opmodes/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SIGMA/opmodes/MotorTest.java
@@ -1,20 +1,24 @@
 package org.firstinspires.ftc.teamcode.SIGMA.opmodes;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.HardwareMap;
+
 @TeleOp(name="MotorTest")
 public class MotorTest extends OpMode {
-    public DcMotor TestMotor;
+    public DcMotor TestMotor1;
+    public DcMotor TestMotor2;
 
     @Override
     public void init() {
-        TestMotor = hardwareMap.get(DcMotor.class, "launchMotor");
+
+        TestMotor1 = hardwareMap.get(DcMotor.class, "launchMotor");
+        TestMotor2 = hardwareMap.get(DcMotor.class, "launchMotor2");
     }@Override
     public void start() {
-        TestMotor.setPower(1);
+
+        TestMotor1.setPower(1);
+        TestMotor2.setPower(-1);
     }
     @Override
     public void init_loop() {
